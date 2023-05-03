6bb2309af2f6117fc55d3747577abb6156de6ca5
saurabhkulkarni12 <51773054+saurabhkulkarni12@users.noreply.github.com>
2023-05-03 12:08:52 +0530

    Update Bootjdbcexample2Application.java

--- src/main/java/com/jdbc/Bootjdbcexample2Application.java
+++ src/main/java/com/jdbc/Bootjdbcexample2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@SpringBootApplication
 public class Bootjdbcexample2Application {
 
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		SpringApplication.run(Bootjdbcexample2Application.class, args);
 	}
 
